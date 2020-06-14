d7a82f15bea46739ae6429392764a45e5d8403fa
Edwin <edwin_ho@me.com>
2020-06-15 09:49:38 +1000

    Update init.zsh as plugin

--- init.zsh
+++ init.zsh
@@ -1,2 +1,10 @@
-source prompt.zsh
-source settings.zsh
+# https://zdharma.org/zinit/wiki/zsh-plugin-standard/
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+# Then ${0:h} to get plugin’s directory
+plugin_path=${0:h}
+echo ${plugin_path}
+
+source ${plugin_path}/prompt.zsh
+source ${plugin_path}/settings.zsh
